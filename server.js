3441fdc6846745723cccf8f2b68cbf649314433e
Hy Huynh <hyxhuynh@gmail.com>
2019-07-08 01:12:38 -0700

    added connection to MongoDB Cloud cluster

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDb connected'))
+  // if failed, incorrect password or did not whitelist IP address
+  .catch(err => console.log(err));
 
 // First route
 app.get('/', (req, res) => res.send('Hello'));
